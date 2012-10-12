4ed93685f76055bfa23c62917634d3f53e900523
Kelsey Martens <kelsey@solspace.com>
2012-10-11 19:50:12 -0500

    Initial commit

---
+++ language/english/lang.primary_category.php
@@ -0,0 +1,117 @@
+<?php
+
+ /**
+ * Solspace - Primary Category
+ *
+ * @package		Solspace:Primary Category
+ * @author		Solspace DevTeam
+ * @copyright	Copyright (c) 2008-2012, Solspace, Inc.
+ * @link		http://solspace.com/docs/addon/c/Primary_Category/
+ * @version		2.0.2
+ * @filesource 	./system/expressionengine/third_party/primary_category/language/english/
+ */
+
+$L = $lang = array(
+
+//----------------------------------------
+// Required for MODULES page
+//----------------------------------------
+
+"primary_category_label" => 
+"Primary Category",
+
+"primary_category_module_name" =>
+"Primary Category",
+
+"primary_category_module_description" =>
+"Allows a primary category to be set",
+
+'primary_category_module_version' =>
+'2.0.0.d8',
+
+//----------------------------------------
+//  Main Menu
+//----------------------------------------
+
+'homepage' =>
+'Homepage',
+
+'documentation' =>
+'Online Documentation',
+//----------------------------------------
+//  Buttons
+//----------------------------------------
+
+'save' =>
+"Save",
+
+//----------------------------------------
+//  Errors
+//----------------------------------------
+
+'invalid_request' =>
+"Invalid Request",
+
+
+"force_primary_cat"					=>
+"Require a primary category for which weblogs?",
+
+"force_primary_cat_channels"					=>
+"Require a primary category for which channels?",
+
+"weblogs"							=>
+"Weblogs",
+
+"channels"							=>
+"Channels",
+
+"settings"							=>
+"Settings",
+
+"channel_settings"					=>
+"Channel Specific Setting",
+
+"weblog_settings"					=>
+"Weblog Specific Setting",
+
+"general_settings"					=>
+"General Settings",
+
+"which_category_groups"				=>
+"Which category groups can be used to assign a primary category?",
+
+"category_groups"					=>
+"Category Groups",
+
+"no_results"						=>
+"No weblogs have been created for this site yet.",
+
+'error_empty'						=>
+'Please assign a primary category to this entry.',
+
+
+"pc_instructions"					=>
+"Assign a primary category for this entry by clicking the corresponding blue arrow on the left.",
+
+"pc_no_cat_group"					=>
+"No category group is assigned to this weblog.",
+
+"remember_primary_category"			=>
+"Remember this primary category for the next new entry.",
+
+"remember_other_categories"			=>
+"Remember '%group_name' categories for the next new entry.",
+
+"collapse"	=>
+"Collapse",
+
+"remember_checkboxes_disabled" =>
+"Disable 'Remember <strong>category choice</strong>' checkboxes?",
+
+"remember_primary_disabled" => 
+"Disable 'Remember <strong>primary category</strong>' checkbox?",
+
+// END
+''=>''
+);
+?>
