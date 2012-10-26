03f95941b90c4f616c7bdbcc3eec55d64157c7df
Emmanuel Beauchamps <e.bops@hotmail.fr>
2012-10-26 23:31:38 +0200

    French language pack
    Translated into French and saved as UTF-8

---
+++ language/french/lang.primary_category.php
@@ -0,0 +1,117 @@
+<?php
+
+ /**
+ * Solspace - Primary Category
+ *
+ * @package		Solspace:Primary Category
+ * @author		Solspace DevTeam
+ * @copyright	Copyright (c) 2008-2012, Solspace, Inc.
+ * @link		http://solspace.com/docs/addon/c/Primary_Category/
+ * @version		2.0.2
+ * @filesource 	./system/expressionengine/third_party/primary_category/language/english/
+ */
+
+$L = $lang = array(
+
+//----------------------------------------
+// Required for MODULES page
+//----------------------------------------
+
+"primary_category_label" =>
+"Catégorie Principale",
+
+"primary_category_module_name" =>
+"Catégorie Principale",
+
+"primary_category_module_description" =>
+"Permet de définir une catégorie principale",
+
+'primary_category_module_version' =>
+'2.0.0.d8', /** version number? */
+
+//----------------------------------------
+//  Main Menu
+//----------------------------------------
+
+'homepage' =>
+'Page d\'accueil',
+
+'documentation' =>
+'Documentation en ligne',
+//----------------------------------------
+//  Buttons
+//----------------------------------------
+
+'save' =>
+"Enregistrer",
+
+//----------------------------------------
+//  Errors
+//----------------------------------------
+
+'invalid_request' =>
+"Demande invalide",
+
+
+"force_primary_cat"					=>
+"Exiger une catégorie principale pour quels blogs ?",
+
+"force_primary_cat_channels"					=>
+"Exiger une catégorie principale pour quels canaux ?",
+
+"weblogs"							=>
+"Blogs",
+
+"channels"							=>
+"Canaux",
+
+"settings"							=>
+"Paramètres",
+
+"channel_settings"					=>
+"Paramétrage du canal",
+
+"weblog_settings"					=>
+"Paramétrage du blog",
+
+"general_settings"					=>
+"Paramètres généraux",
+
+"which_category_groups"				=>
+"Quels groupes de catégories peuvent être utilisés afin d'assigner une catégorie principale ?",
+
+"category_groups"					=>
+"Groupes de catégories",
+
+"no_results"						=>
+"Aucun canal n'a encore été créé pour ce site.",
+
+'error_empty'						=>
+'Merci d\'assigner une catégorie principale à cette entrée.',
+
+
+"pc_instructions"					=>
+"Assigner une catégorie principale à cette entrée en cliquant à gauche sur la flèche bleue correspondante.",
+
+"pc_no_cat_group"					=>
+"Aucun groupe de catégories n'est assigné à ce canal.",
+
+"remember_primary_category"			=>
+"Mémoriser cette catégorie principale pour l'entrée suivante.",
+
+"remember_other_categories"			=>
+"Mémoriser les catégories de '%group_name' pour l'entrée suivante.",
+
+"collapse"	=>
+"Réduire",
+
+"remember_checkboxes_disabled" =>
+"Désactiver les cases à cocher 'Mémoriser <strong>le choix de catégorie</strong>' ?",
+
+"remember_primary_disabled" =>
+"Désactiver la case à cocher 'Mémoriser <strong>la catégorie principale</strong>' ?",
+
+// END
+''=>''
+);
+?>
